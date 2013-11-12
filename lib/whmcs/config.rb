783d2f3ef35cba8233edda8ae79c18f7a9ca196f
andreyv <jujav4ik@gmail.com>
2013-11-12 12:47:42 +0200

    added support for API access key authorization

--- lib/whmcs/config.rb
+++ lib/whmcs/config.rb
@@ -10,11 +10,15 @@ module WHMCS
     # The WHMCS API URL
     attr_accessor :api_url
 
+	# The WHMCS API Access Key
+	attr_accessor :api_access_key
+
     # Create a new config object
     def initialize
       @api_username = 'example_api_user'
       @api_password = 'example_api_pass'
       @api_url      = 'http://example.com/api.php'
+	  @api_access_key = '3x4mpL3_k3y'
     end
   end
 end
